f684b17b8f5a88b8adeaa86ae9c0f7a737bdbe79
Arsalan Mustafa Malik <149749819+malik123arsalan@users.noreply.github.com>
2026-07-27 06:29:51 +0000

    Added Pydantic model and 3 incident endpoints

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ class Incident(BaseModel):
     problem_detail: str
     severity: str
 
+incidents = []
+
 @app.get("/")
 def home():
     return {"message": "ResolveX API is live and working"}
+
+@app.post("/incidents")
+def create_incident(incident: Incident):
+    incidents.append(incident)
+    return {"message": "Incident added successfully", "data": incident}
+
+@app.get("/incidents")
+def get_all_incidents():
+    return incidents
+
+@app.get("/incidents/{incident_id}")
+def get_incident_by_id(incident_id: int):
+    for incident in incidents:
+        if incident.id == incident_id:
+            return incident
+    return {"error": "Incident not found"}
